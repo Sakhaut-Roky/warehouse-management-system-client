8e72155ff18af010ebf40c16968e6da6a542d908
Md. Sakhaut Hossin <sakhaut.roky18@gmail.com>
2022-05-29 17:07:07 +0600

    jwt token

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -68,7 +68,7 @@ const Blogs = () => {
                         JWT stands for JSON Web Token where JSON stands for JavaScript Object Notation.
                     </h5>
                     <h5>
-                    JSON Web Token (JWT) is an open standard that defines a compact and self-contained way for securely transmitting information between parties as a JSON object. This information can be verified and trusted because it is digitally signed. JWTs can be signed using a secret or a public/private key pair.
+                    JSON Web Token is an open standard that defines a compact and self-contained way for securely transmitting information between parties as a JSON object. This information can be verified and trusted because it is digitally signed. JWTs can be signed using a secret or a public/private key pair.
                     </h5>
                     <h5>
                     When should you use JSON Web Tokens?
